7e1f9f0d7dd9c21a5aea490a25a8f88156c40f37
Lord-Y <Lord-Y@users.noreply.github.com>
2025-11-12 15:28:39 +0100

    docs(specs): Add build_spec script

---
+++ docs/build_spec.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [ -n "$1" ]
+then
+  export VERSION="$1"
+else
+  echo "VERSION argument is mantatory"
+  echo "Example: $0 0.4"
+  exit 1
+fi
+shift
+
+ROOT_DIR="specs/design/${VERSION}"
+FILE_NAME="DESIGN_DOC_v${VERSION}.md"
+GLOBAL_FILE_NAME="specs/${FILE_NAME}"
+SPEC_FILE_NAME="${ROOT_DIR}/${FILE_NAME}"
+DIRECTORIES="syntax semantics design_principles appendix"
+FOOTER="---\n\n© $(date +%Y) Ori Language — Design Spec"
+
+> ${GLOBAL_FILE_NAME}
+> ${SPEC_FILE_NAME}
+
+for file in $(find ${ROOT_DIR} -maxdepth 1 -type f -name "*.md" |grep -vE "README.md|${FILE_NAME}" | sort)
+do
+  echo "${file}"
+  cat ${file} >> ${SPEC_FILE_NAME}
+  >> ${SPEC_FILE_NAME}
+done
+
+for dir in ${DIRECTORIES}
+do
+  for file in $(find ${ROOT_DIR}/${dir} -type f -name "*.md" | sort)
+  do
+    echo "${file}"
+    cat ${file} >> ${SPEC_FILE_NAME}
+    echo -e "" >> ${SPEC_FILE_NAME}
+    echo -e "" >> ${SPEC_FILE_NAME}
+  done
+done
+
+echo -e ${FOOTER} >> ${SPEC_FILE_NAME}
+cp ${SPEC_FILE_NAME} ${GLOBAL_FILE_NAME}
+
